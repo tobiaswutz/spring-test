14eb5b07c62499191c5bb3100439f7c3e9b118f1
Tobias Wutz <tobiaswutz22@gmail.com>
2024-08-21 13:49:52 +0200

    server log

--- src/main/java/com/example/demo/HelloController.java
+++ src/main/java/com/example/demo/HelloController.java
@@ -1,16 +1,21 @@
 package com.example.demo;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
 
+  private static final Logger logger = LoggerFactory.getLogger(HelloController.class);
   private int callCount = 0;
 
   @GetMapping("/hello/callCount")
   public String getCallCount() {
     callCount++;
+    // Logge die Anzahl der Aufrufe
+    logger.info("Endpoint /hello/callCount wurde {} mal aufgerufen.", callCount);
     return "Endpoint /hello wurde " + callCount + " mal aufgerufen.";
   }
 
